71f0c9500201d9bdb3dd9391bd96c6ac7b8407ad
Misha <mishap635@mail.com>
2025-03-18 16:46:49 +0200

    frfr

--- new.js
+++ new.js
@@ -1,3 +1,10 @@
-console.log("Hello world")
-fdsscas
-xascsa
+const x = 0.1;
+const y = 0.2;
+let z = x + y;
+console.log(z)
+console.log((0.17*100 + 0.24*100)/100)
+console.log((0.17+0.24).toFixed(1))
+
+const number = -4.56;
+const absoluteValue = Math.abs(number);
+console.log(absoluteValue)
